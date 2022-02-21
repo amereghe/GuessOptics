37675f4d6faed0a61b99f61011bb61ac6312cf4a
Mereghetti Alessio <Alessio.Mereghetti@Main.Cnao.group>
2022-02-21 16:35:10 +0100

    adding analysis of single scan and of multiple scans

--- analysisMe.m
+++ analysisMe.m
@@ -182,10 +182,14 @@ CreateMeasFiles(pathQuaDipDip,HEBTmagnetNames,[20 4 4 4],[1 1 1 1],magnetNames,L
 
 %% Parse distributions
 % - some user variables
+beamPart="PROTON";
 LPOWmonPath="LPOW_error_log";
-machine="LineZ";
 config="TM"; % select configuration: TM, RFKO
 dataParentPath="scambio\Alessio\GuessOptics\data";
+machines=[ "LineZ" "LineZ" "LineZ" "LineZ" "LineZ" "LineZ" ...
+           "LineV" "LineV" "LineV" "LineV" ...
+           "LineU" "LineU" "LineT" "LineT" "LineZ" "LineZ" ...
+           "LineU"];
 profileDataPaths=[ 
     "090mm\HEBT\alone_H2-016A-QUE\PRC-544-220109-0357_H5-002B-SFM" ...
     "090mm\HEBT\alone_H2-022A-QUE\PRC-544-220108-2243_H3-011A-QBM" ...
@@ -198,6 +202,11 @@ profileDataPaths=[
     "090mm\HEBT\alone_H4-013A-QUE\PRC-544-220108-2330_V1-007B-SFM" ...
     "090mm\HEBT\alone_V1-001A-SWV\PRC-544-220109-0022_V1-007B-SFM" ...
     "090mm\HEBT\alone_H5-009A-QUE\PRC-544-220109-0259_U1-021B-SFM" ...
+    "090mm\HEBT\alone_H5-009A-QUE\PRC-544-220109-0303_U1-005B-SFM" ...
+    "090mm\HEBT\alone_H5-009A-QUE\PRC-544-220109-0316_T1-007B-SFM" ...
+    "090mm\HEBT\alone_H5-009A-QUE\PRC-544-220109-0321_T1-016B-SFM" ...
+    "090mm\HEBT\alone_H5-009A-QUE\PRC-544-220109-0329_Z1-007B-SFM" ...
+    "090mm\HEBT\alone_H5-009A-QUE\PRC-544-220109-0334_Z1-016B-SFM" ...
     "090mm\HEBT\alone_H5-015A-QUE\PRC-544-220109-0254_U1-021B-SFM" ...
     ];
 scanMagnets=[
@@ -205,21 +214,24 @@ scanMagnets=[
     "H2-022A-QUE" "H2-022A-QUE" "H2-022A-QUE" "H2-022A-QUE" ... %
     "H3-003A-SW2"                                           ... % 
     "H4-003A-QUE" "H4-007A-QUE" "H4-013A-QUE" "V1-001A-SWV" ... % setting up V1 combined scans
-    "H5-009A-QUE" "H5-015A-QUE" ...                             % setting up H5-T1/U1/Z1 combined scans
+    "H5-009A-QUE" "H5-009A-QUE" "H5-009A-QUE" "H5-009A-QUE" "H5-009A-QUE" "H5-009A-QUE" ...  % setting up H5-T1/U1/Z1 combined scans
+    "H5-015A-QUE" 
     ];
 parMagnets=[
     missing() ...
     missing()      missing()     missing()     missing() ...
     missing() ...
     missing()      missing()     missing()     missing() ...
-    missing()      missing() ...
+    missing()      missing()     missing()     missing()     missing()     missing() ...
+    missing()
     ];
 monitors=[
     "H5-002B-SFM" ...
     "H3-011A-QBM" "H3-011A-QBM" "H4-010B-SFM" "H4-018B-SFM" ...
     "H5-002B-SFM" ...
     "V1-007B-SFM" "V1-007B-SFM" "V1-007B-SFM" "V1-007B-SFM" ...
-    "U1-021B-SFM" "U1-021B-SFM" ...
+    "U1-021B-SFM" "U1-005B-SFM" "T1-007B-SFM" "T1-016B-SFM" "Z1-007B-SFM" "Z1-016B-SFM" ...
+    "U1-021B-SFM" ...
     ];
 currentFiles=[
     "090mm\HEBT\alone_H2-016A-QUE\LGEN_PROTON_alone_H2-016A-QUE.xls"
@@ -228,6 +240,17 @@ currentFiles=[
     "090mm\HEBT\alone_H2-022A-QUE\LGEN_PROTON_alone_H2-022A-QUE.xls"
     "090mm\HEBT\alone_H2-022A-QUE\LGEN_PROTON_alone_H2-022A-QUE.xls"
     "090mm\HEBT\alone_H3-003A-SW2\LGEN_PROTON_alone_H3-003A-SW2.xls"
+    ""
+    ""
+    ""
+    ""
+    "090mm\HEBT\alone_H5-009A-QUE\LGEN_PROTON_alone_H5-009A-QUE.xls"
+    "090mm\HEBT\alone_H5-009A-QUE\LGEN_PROTON_alone_H5-009A-QUE.xls"
+    "090mm\HEBT\alone_H5-009A-QUE\LGEN_PROTON_alone_H5-009A-QUE.xls"
+    "090mm\HEBT\alone_H5-009A-QUE\LGEN_PROTON_alone_H5-009A-QUE.xls"
+    "090mm\HEBT\alone_H5-009A-QUE\LGEN_PROTON_alone_H5-009A-QUE.xls"
+    "090mm\HEBT\alone_H5-009A-QUE\LGEN_PROTON_alone_H5-009A-QUE.xls"
+    "090mm\HEBT\alone_H5-009A-QUE\LGEN_PROTON_alone_H5-009A-QUE.xls"
     ];
 indices=zeros(2,2,length(profileDataPaths));
 indices(1,:,1)=[3,33]; indices(2,:,1)=[1,31];
@@ -236,9 +259,23 @@ indices(1,:,3)=[3,43]; indices(2,:,3)=[1,41];
 indices(1,:,4)=[3,43]; indices(2,:,4)=[1,41];
 indices(1,:,5)=[3,43]; indices(2,:,5)=[1,41];
 indices(1,:,6)=[3,17]; indices(2,:,6)=[1,15];
+indices(1,:,11)=[3,43]; indices(2,:,11)=[1,41];
+indices(1,:,12)=[3,43]; indices(2,:,12)=[1,41];
+indices(1,:,13)=[3,43]; indices(2,:,13)=[1,41];
+indices(1,:,14)=[3,43]; indices(2,:,14)=[1,41];
+indices(1,:,15)=[3,43]; indices(2,:,15)=[1,41];
+indices(1,:,16)=[3,43]; indices(2,:,16)=[1,41];
+%
+fitIndices=zeros(2,2,length(profileDataPaths)); % 1: min/max; 2:hor/ver; 3:measurement;
+fitIndices(:,1,11)=[ 27 37 ]; fitIndices(:,2,11)=[ 18 25 ];
+fitIndices(:,1,12)=[ 27 41 ]; fitIndices(:,2,12)=[ 10 23 ];
+fitIndices(:,1,13)=[ 28 41 ]; fitIndices(:,2,13)=[ 13 27 ];
+fitIndices(:,1,14)=[ 24 34 ]; fitIndices(:,2,14)=[ 16 27 ];
+fitIndices(:,1,15)=[ 29 41 ]; fitIndices(:,2,15)=[ 13 27 ];
 % 
+%% single set
 % - select data set
-iSet=6;
+iSet=15;
 %
 % - inflate some variables
 if ( ismissing(parMagnets(iSet)) )
@@ -256,22 +293,36 @@ LPOWmonPaths=sprintf("%s\\%s\\%s\\2022-01-*\\*.txt",kPath,dataParentPath,LPOWmon
 [selectedLGENnames,indLGENs]=ReturnLGENnames(magnetNames,LGENnames,scanMagnets(iSet));
 %
 % CLEAR EXISTING DATA
-clear measData,cyCodes,cyProgs,sumData,BARs,FWHMs,INTs,IsXLS,LGENnamesXLS,nDataCurr,tableIs;
+clear measData,cyCodes,cyProgs,sumData,BARs,FWHMs,INTs,LGENnamesXLS,nDataCurr;%,IsXLS,tableIs;
 %
 % ACTUALLY DO THE JOB!
 [measData,cyCodes,cyProgs]=...                           % parse SFM/QBM data
     ParseSFMData(sprintf("%s\\Data*%s.csv",dataPath,monType),monType);
 sumData=SumSpectra(measData);                            % get summary profiles
-ShowSpectra(sumData,myTitle,cyProgs,"cyProg");           % 3D plots
+ShowSpectra(sumData,myTitle);                            % 3D plots
+% ShowSpectra(sumData,myTitle,cyProgs,"cyProg");           % 3D plots
 saveas(gcf,myFigName);
 [BARs,FWHMs,INTs]=StatDistributionsBDProcedure(sumData); % first statistical analysis (as BD procedure)
+tmpX=reshape(measData(:,1,1:2,1),[size(measData,1) 2]);
+myMask=ReturnMask(iSet,tmpX);
+% myMask=ReturnMask(-1,tmpX);
+fracEst=0.5:0.1:0.9;
+[AdvBARs,AdvFWHMs,AdvINTs,advFWxMls,advFWxMrs]=StatDistributions(sumData,fracEst,0.12,missing(),myMask);
+ShowScanRawPlots(missing(),AdvFWHMs(:,:,fracEst==0.5),AdvBARs,AdvINTs,size(AdvFWHMs,1),scanMagnets(iSet),monitors(iSet));
+FWxMPlots(missing(),AdvFWHMs,AdvBARs,fracEst,indices(:,:,iSet),sprintf("%s - FWxM",scanMagnets(iSet)),monitors(iSet));
+FWxMPlots(missing(),advFWxMls,AdvBARs,fracEst,indices(:,:,iSet),sprintf("%s - left HWxM",scanMagnets(iSet)),monitors(iSet),false);
+FWxMPlots(missing(),advFWxMrs,AdvBARs,fracEst,indices(:,:,iSet),sprintf("%s - right HWxM",scanMagnets(iSet)),monitors(iSet),false);
+compBARs=BARs; compSigmas=GetReducedFWxM(FWHMs,0.5,true); compINTs=INTs; compIndices=indices(2,:,iSet);
+compBARs(:,:,2)=AdvBARs; compSigmas(:,:,2)=GetReducedFWxM(AdvFWHMs(:,:,fracEst==0.8),0.8,true); compINTs(:,:,2)=AdvINTs;
+CompareStats(compBARs,compSigmas,compINTs,compIndices,["BD" "adv"]);
 % - acquire .xls of scan
 currentPath=sprintf("%s\\%s\\%s\\%s",kPath,dataParentPath,beamPart,currentFiles(iSet));
 [IsXLS,LGENnamesXLS,nDataCurr]=AcquireCurrentData(currentPath);
 % - show raw data
-ShowScanRawPlots(IsXLS(1:nDataCurr,LGENnamesXLS==selectedLGENnames),FWHMs,BARs,INTs,size(FWHMs,1),scanMagnets(iSet),monitors(iSet));%,myFigName);
+ShowScanRawPlots(IsXLS(1:nDataCurr,LGENnamesXLS==selectedLGENnames),FWHMs,BARs,INTs,size(FWHMs,1),scanMagnets(iSet),monitors(iSet));
+ShowScanRawPlots(IsXLS(1:nDataCurr,LGENnamesXLS==selectedLGENnames),FWHMs,BARs,INTs,size(FWHMs,1),scanMagnets(iSet),monitors(iSet),myFigName);
 % - get TM currents
-[cyCodesTM,rangesTM,EksTM,BrhosTM,currentsTM,fieldsTM,kicksTM,psNamesTM,FileNameCurrentsTM]=AcquireLGENValues(beamPart,machine,config);
+[cyCodesTM,rangesTM,EksTM,BrhosTM,currentsTM,fieldsTM,kicksTM,psNamesTM,FileNameCurrentsTM]=AcquireLGENValues(beamPart,machines(iSet),config);
 psNamesTM=string(psNamesTM);
 cyCodesTM=upper(string(cyCodesTM));
 % - parse LPOW monitor log
@@ -283,7 +334,244 @@ end
 % - build table of currents
 [tableIs]=BuildCurrentTable(cyCodes,cyProgs,selectedLGENnames,IsXLS,LGENnamesXLS,psNamesTM,cyCodesTM,currentsTM,LGENsLPOWMon,cyProgsLPOWMon,appValsLPOWMon,indices(:,:,iSet));
 % - show data aligned
-ShowScanAligned(IsXLS(1:nDataCurr,LGENnamesXLS==selectedLGENnames),FWHMs,BARs,indices(:,:,iSet),scanMagnets(iSet),monitors(iSet));%,myFigName);
+ShowScanAligned(IsXLS(1:nDataCurr,LGENnamesXLS==selectedLGENnames),FWHMs,BARs,indices(:,:,iSet),scanMagnets(iSet),monitors(iSet),myFigName);
+ShowScanAligned(IsXLS(1:nDataCurr,LGENnamesXLS==selectedLGENnames),AdvFWHMs,AdvBARs,indices(:,:,iSet),scanMagnets(iSet),monitors(iSet));
+
+return
+for ii=1:size(measData,4)
+    ShowSpectra(measData(:,:,:,ii),myTitle);
+    pause();
+end
+myID=2;
+ShowSpectra(measData(:,:,:,myID),sprintf("%s - ID=%d",myTitle,myID));
+myID=38;
+ShowSpectra(measData(:,:,:,myID),sprintf("%s - ID=%d",myTitle,myID));
+
+%% aggregati di scansioni - acquisisci dati
+nRows=1; nCols=3; nSetsPerPlot=2;
+mySets=zeros(nRows,nCols,nSetsPerPlot);
+mySets(1,1,1)=15; mySets(1,1,2)=16; % lineZ
+mySets(1,2,1)=12; mySets(1,2,2)=11; % lineU
+mySets(1,3,1)=13; mySets(1,3,2)=14; % lineT
+myMonitors=strings(nRows,nCols,nSetsPerPlot);
+% myIndices=zeros(2,nRows,nCols,nSetsPerPlot);
+for iRowSet=1:nRows
+    for iColSet=1:nCols
+        for iDataSet=1:nSetsPerPlot
+            myMonitors(iRowSet,iColSet,iDataSet)=monitors(mySets(iRowSet,iColSet,iDataSet));
+            % myIndices(:,iRowSet,iColSet,iDataSet)=indices(2,:,mySets(iRowSet,iColSet,iDataSet));
+        end
+    end
+end
+
+lFirst=true;
+nMeasData=zeros(size(mySets));
+% % CLEAR EXISTING DATA
+% clear measData cyCodes cyProgs sumData BARs FWHMs INTs IsXLS LGENnamesXLS nDataCurr;
+measData=NaN(); cyCodes=strings(1); cyProgs=NaN(); sumData=NaN(); BARs=NaN(); FWHMs=NaN(); INTs=NaN(); IsXLS=NaN(); LGENnamesXLS=NaN(); nDataCurr=NaN(); 
+for iRowSet=1:nRows
+    for iColSet=1:nCols
+        for iDataSet=1:nSetsPerPlot
+            if ( ~lFirst ), clear tmpMeasData tmpCyCodes tmpCyProgs tmpSumData tmpBARs tmpFWHMs tmpINTs; end
+            % acquire profiles
+            dataPath=sprintf("%s\\%s\\%s\\%s",kPath,dataParentPath,beamPart,profileDataPaths(mySets(iRowSet,iColSet,iDataSet)));
+            monType=upper(split(monitors(iSet),"-")); monType=monType(end);
+            % parse SFM/QBM data
+            [tmpMeasDataT,tmpCyCodesT,tmpCyProgsT]=...
+                ParseSFMData(sprintf("%s\\Data*%s.csv",dataPath,monType),monType);
+            
+            % select only actual data set
+            myFilterMeasIDs=indices(2,1,mySets(iRowSet,iColSet,iDataSet)):indices(2,2,mySets(iRowSet,iColSet,iDataSet));
+            tmpMeasData=tmpMeasDataT(:,:,:,myFilterMeasIDs);
+            tmpCyCodes=tmpCyCodesT(myFilterMeasIDs);
+            tmpCyProgs=tmpCyProgsT(myFilterMeasIDs);
+            
+            % get summary profiles
+            tmpSumData=SumSpectra(tmpMeasData);
+            % first statistical analysis (as BD procedure)
+            [tmpBARs,tmpFWHMs,tmpINTs]=StatDistributionsBDProcedure(tmpSumData);
+            
+            % acquire .xls of scan
+            if ( lFirst )
+                currentPath=sprintf("%s\\%s\\%s\\%s",kPath,dataParentPath,beamPart,currentFiles(mySets(iRowSet,iColSet,iDataSet)));
+                [IsXLSt,LGENnamesXLS,nDataCurr]=AcquireCurrentData(currentPath);
+                % select only actual data set
+                IsXLS=IsXLSt(indices(1,1,mySets(iRowSet,iColSet,iDataSet)):indices(1,2,mySets(iRowSet,iColSet,iDataSet)));
+                % myIndicesIs=indices(1,:,mySets(iRowSet,iColSet,iDataSet));
+                myScanMagnet=scanMagnets(mySets(iRowSet,iColSet,iDataSet));
+                lFirst=false;
+            end
+            
+            % storage
+            measData=CopyMe(tmpMeasData,measData,iRowSet,iColSet,iDataSet);
+            cyCodes=CopyMe(tmpCyCodes,cyCodes,iRowSet,iColSet,iDataSet);
+            cyProgs=CopyMe(tmpCyProgs,cyProgs,iRowSet,iColSet,iDataSet);
+            sumData=CopyMe(tmpSumData,sumData,iRowSet,iColSet,iDataSet);
+            BARs=CopyMe(tmpBARs,BARs,iRowSet,iColSet,iDataSet);
+            FWHMs=CopyMe(tmpFWHMs,FWHMs,iRowSet,iColSet,iDataSet);
+            INTs=CopyMe(tmpINTs,INTs,iRowSet,iColSet,iDataSet);
+        end
+    end
+end
+
+%% aggregati di scansioni - mostra dati
+% CompareSpectraSingleShot(IsXLS,sumData,myScanMagnet,myMonitors);%,myIndices,myIndicesIs);
+CompareSpectra(IsXLS,sumData,myScanMagnet,myMonitors);%,myIndices,myIndicesIs);
+
+%% aggregati di scans - acquisisci dati
+mySets=[ 15 12 11 13 14 ]; 
+myMonitors=monitors(mySets);
+fracEst=0.5:0.1:0.9;
+% CLEAR EXISTING DATA
+clear measData cyCodes cyProgs sumData AdvBARs AdvFWHMs AdvINTs advFWxMls advFWxMrs;
+for iMySet=1:length(mySets)
+    % acquire profiles
+    dataPath=sprintf("%s\\%s\\%s\\%s",kPath,dataParentPath,beamPart,profileDataPaths(mySets(iMySet)));
+    monType=upper(split(myMonitors(iMySet),"-")); monType=monType(end);
+    % parse SFM/QBM data
+    [tmpMeasDataT,tmpCyCodesT,tmpCyProgsT]=...
+        ParseSFMData(sprintf("%s\\Data*%s.csv",dataPath,monType),monType);
+
+    % select only actual data set
+    myFilterMeasIDs=indices(2,1,mySets(iMySet)):indices(2,2,mySets(iMySet));
+    measData(:,:,:,:,iMySet)=tmpMeasDataT(:,:,:,myFilterMeasIDs);
+    cyCodes(:,iMySet)=tmpCyCodesT(myFilterMeasIDs);
+    cyProgs(:,iMySet)=tmpCyProgsT(myFilterMeasIDs);
+
+    % get summary profiles
+    sumData(:,:,:,iMySet)=SumSpectra(measData(:,:,:,:,iMySet));
+    % statistical analysis
+    tmpX=reshape(measData(:,1,1:2,1,iMySet),[size(measData(:,1,1:2,1,iMySet),1) 2]);
+    myMask=ReturnMask(mySets(iMySet),tmpX);
+    [AdvBARs(:,:,iMySet),AdvFWHMs(:,:,:,iMySet),AdvINTs(:,:,iMySet),advFWxMls(:,:,:,iMySet),advFWxMrs(:,:,:,iMySet)]=StatDistributions(sumData(:,:,:,iMySet),fracEst,0.12,missing(),myMask,false);
+    
+end
+% acquire .xls of scan
+currentPath=sprintf("%s\\%s\\%s\\%s",kPath,dataParentPath,beamPart,currentFiles(mySets(iMySet)));
+[IsXLSt,LGENnamesXLS,nDataCurr]=AcquireCurrentData(currentPath);
+% select only actual data set
+IsXLS=IsXLSt(indices(1,1,mySets(iMySet)):indices(1,2,mySets(iMySet)));
+myScanMagnet=scanMagnets(mySets(iMySet));
+
+%%
+figure();
+planes=["HOR" "VER"];
+for iPlane=1:length(planes)
+    subplot(1,2,iPlane);
+    for iMySet=1:length(mySets)
+        if ( iMySet>1 ), hold on; end
+        myFilterDs=fitIndices(1,iPlane,mySets(iMySet)):fitIndices(2,iPlane,mySets(iMySet));
+        plot(IsXLS(myFilterDs),GetReducedFWxM(AdvFWHMs(myFilterDs,iPlane,fracEst==0.8,iMySet),0.8,true),"*-");
+        grid on; xlabel("I [A]"); ylabel("\sigma [mm]"); legend(myMonitors,"Location","best");
+    end
+    title(sprintf("%s plane",planes(iPlane)));
+end
+sgtitle(sprintf("%s",LabelMe(myScanMagnet)));
+
+function CompareSpectraSingleShot(IsXLS,sumData,myScanMagnet,myMonitors,myIndices)%,myIndices,myIndicesIs)
+    planes=[ "HOR plane" "VER plane"];
+    if ( ~exist('myIndices','var') ), myIndices=1:size(sumData,2)-1; end
+    nRows=size(sumData,4);
+    nCols=size(sumData,5);
+    nSetsPerPlot=size(sumData,6);
+    ff=figure();
+    % iCurr=myIndicesIs(1):myIndicesIs(2);
+    % yMax=max(sumData,[],"all"); yMin=0;
+    for iScan=myIndices%length(iCurr)
+        for iPlane=1:2
+            yMax=max(sumData(:,1+iScan,iPlane,:,:,:),[],"all"); yMin=0;
+            if ( iPlane==1 ), iPlot=0; else iPlot=nCols*nRows; end
+            for iRowSet=1:nRows
+                for iColSet=1:nCols
+                    iPlot=iPlot+1;
+                    subplot(2*nRows,nCols,iPlot);
+                    for iDataSet=1:nSetsPerPlot
+                        if ( iDataSet>1 ), hold on; end
+                        % mySel=myIndices(1,iRowSet,iColSet,iDataSet)+iScan-1;
+                        % plot(sumData(:,1,iPlane,iRowSet,iColSet,iDataSet),sumData(:,1+mySel,iPlane,iRowSet,iColSet,iDataSet),"*-");
+                        plot(sumData(:,1,iPlane,iRowSet,iColSet,iDataSet),sumData(:,1+iScan,iPlane,iRowSet,iColSet,iDataSet),"*-");
+                    end
+                    grid on; xlabel("fiber position [mm]"); ylabel("counts []"); title(planes(iPlane));
+                    legend(myMonitors(iRowSet,iColSet,:),"Location","best");
+                    ylim([yMin yMax]);
+                    hold off;
+                end
+            end
+        end
+        % sgtitle(sprintf("I_{%s}=%g A",LabelMe(myScanMagnet),IsXLS(iCurr(iScan))));
+        sgtitle(sprintf("I_{%s}=%g A (ID=%d)",LabelMe(myScanMagnet),IsXLS(iScan),iScan));
+        pause();
+    end
+end
+
+function CompareSpectra(IsXLS,sumData,myScanMagnet,myMonitors,myIndices)%,myIndices,myIndicesIs)
+    planes=[ "HOR plane" "VER plane"];
+    if ( ~exist('myIndices','var') ), myIndices=1:size(sumData,2)-1; end
+    nRows=size(sumData,4);
+    nCols=size(sumData,5);
+    nSetsPerPlot=size(sumData,6);
+    cm=colormap(parula(length(myIndices)));
+    % iCurr=myIndicesIs(1):myIndicesIs(2);
+    % yMax=max(sumData,[],"all"); yMin=0;
+    for iPlane=1:2
+        ff=figure();
+        iPlot=0;
+        for iRowSet=1:nRows
+            for iDataSet=1:nSetsPerPlot
+                for iColSet=1:nCols
+                    iPlot=iPlot+1;
+                    subplot(nRows*nSetsPerPlot,nCols,iPlot);
+                    for iScan=myIndices
+                        if ( iScan>1 ), hold on; end
+                        Plot3D(sumData(:,1,iPlane,iRowSet,iColSet,iDataSet),sumData(:,1+iScan,iPlane,iRowSet,iColSet,iDataSet),iScan,cm(iScan,:));
+                    end
+                    grid on; xlabel("[mm]"); ylabel("ID []"); zlabel("counts []"); title(myMonitors(iRowSet,iColSet,iDataSet));
+                end
+            end
+        end
+        sgtitle(sprintf("%s - %s",LabelMe(myScanMagnet),planes(iPlane)));
+    end
+end
+
+function Plot3D(xx,yy,iSet,color)
+    plotX=xx;
+    if ( size(plotX,2)== 1 )
+        plotX=plotX';
+    end
+    plotY=yy;
+    if ( size(plotY,2)== 1 )
+        plotY=plotY';
+    end
+    % get non-zero values
+    indices=(plotY~=0.0);
+    plotX=plotX(indices);
+    plotY=plotY(indices);
+    nn=size(plotX,2);
+    zz=iSet*ones(1,nn);
+    plot3(plotX,zz,plotY,"color",color);%,'FaceAlpha',0.3,'EdgeColor',color);
+end
+
+function [matrOut]=CopyMe(matrIn,matrOut,iRowSet,iColSet,iDataSet)
+    switch length(size(matrIn))
+        case 2
+            if ( size(matrIn,2)==1 )
+                % actually, one dimension only
+                matrOut(1:size(matrIn,1),iRowSet,iColSet,iDataSet)= ...
+                 matrIn(1:size(matrIn,1));
+            else
+                matrOut(1:size(matrIn,1),1:size(matrIn,2),iRowSet,iColSet,iDataSet)= ...
+                 matrIn(1:size(matrIn,1),1:size(matrIn,2));
+            end
+        case 3
+            matrOut(1:size(matrIn,1),1:size(matrIn,2),1:size(matrIn,3),iRowSet,iColSet,iDataSet)=...
+             matrIn(1:size(matrIn,1),1:size(matrIn,2),1:size(matrIn,3));
+        case 4
+            matrOut(1:size(matrIn,1),1:size(matrIn,2),1:size(matrIn,3),1:size(matrIn,4),iRowSet,iColSet,iDataSet)=...
+             matrIn(1:size(matrIn,1),1:size(matrIn,2),1:size(matrIn,3),1:size(matrIn,4));
+        otherwise
+            error("...please increase number of handled dimensions!");
+    end
+end
 
 %% tests vari
 
